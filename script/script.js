66ba838e5583a807dd762beaa1d0982b8bf98fa0
Vinícius Pires <81810017+ViniciusJPSilva@users.noreply.github.com>
2023-05-04 20:52:20 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,10 +2,18 @@
 const bgMusic = document.querySelector("#bg-music")
 const playButton = document.querySelector("#play-music");
 
-playButton.addEventListener('click', function() {
-    document.body.style.background = "url(\"img/daft_punk.gif\") no-repeat fixed";
-    document.body.style.backgroundSize = "cover";
+playButton.addEventListener("click", function() {
+    changeBackground();
     playButton.style.display = "none";
     bgMusic.currentTime = 64;
     bgMusic.play();
 });
+
+addEventListener("resize", function() {
+    changeBackground();
+});
+
+function changeBackground() {
+    document.body.style.background = "url(\"img/daft_punk.gif\") no-repeat";
+    document.body.style.backgroundSize = window.innerWidth + "px " + window.innerHeight + "px";
+};
